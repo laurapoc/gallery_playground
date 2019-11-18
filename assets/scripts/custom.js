bf755b41ea4744ce90d910df210090b4937e98a8
laurapoc <laura.pociuniene@gmail.com>
2019-11-18 19:53:33 +0200

    Basic gallery playground

---
+++ assets/scripts/custom.js
@@ -0,0 +1,29 @@
+"use strict";
+
+$('[data-fancybox="gall"]').fancybox({
+  loop: true,
+  animationDuration: 1000
+});
+
+
+let sort = document.querySelector(".gallery_toggle");
+let galleryMenu = document.querySelector(".photo");
+let showNum = 3;
+sort.addEventListener("click", function() {
+  if (showNum < galleryMenu.children.length) {
+    showNum = showNum + 3;
+    for (let i = 0; i < showNum; i++) {
+      galleryMenu.children[i].classList.add("show");
+      galleryMenu.children[i].children[0].classList.add("enableTransition");
+    }
+  } else {
+    for (let i = 0; i < showNum; i++) {
+      galleryMenu.children[i].classList.remove("show");
+      galleryMenu.children[i].children[0].classList.remove("enableTransition");
+    }
+    showNum = 3;
+  }
+});
+
+
+
